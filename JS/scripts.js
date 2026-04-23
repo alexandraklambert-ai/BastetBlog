4bbf3f55bbf51d0f2a03e14e2228d7f3589b50f4
alexandraklambert-ai <alexandraklambert@gmail.com>
2026-04-23 17:39:48 +0200

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('.menu-toggle').on('click', function(){
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+    });
+
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4500,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 880,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 570,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
